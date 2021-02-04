aa41befc0233df2984c2b60de148286b4d7917af
john <johnageronimo1@gmail.com>
2021-02-05 11:55:06 +1300

    constructor added to app js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,33 @@
-import "./App.css";
+import React, { Component } from 'react'
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 'id1'}]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
-      </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
