3bbbb1a472bd51adc16931c2e550cc0235c127a8
GeronimoJohn <johnageronimo1@gmail.com>
2021-02-04 23:20:16 +1300

    implented the front end look

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Tracklist.css";
+
+const Tracklist = () => {
+  return (
+    <div class="TrackList">
+      {/* <!-- You will add a map method that renders a set of Track components  --> */}
+    </div>
+  );
+};
+
+export default Tracklist;
